9f332afad88a938eec0af65a7b58855e936c18cf
noah-10 <noah.fryman.10@gmail.com>
2024-02-02 17:15:03 -0500

    Added a helper folder with functions for writing, reading and appending content. Also, wrote functionality for the server and the index.js

---
+++ Develop/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const { log } = require('./middleware/log.js');
+const api = require('./routes/index.js');
+
+const PORT = 3001;
+const app = express();
+
+// Custom middle to log interactions
+app.use(log);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('api', api);
+
+app.use(express.static('public'));
+
+// Get route for homepage
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+// Get route for notes page
+app.get('/note', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening at http://localhost:${PORT}`);
+})
